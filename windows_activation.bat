ca66b68e5f9f5b626207bca9c0e2ebb5d5f5a09d
sneekysnickers <71842325+sneekysnickers@users.noreply.github.com>
2025-01-17 13:55:59 +0100

    Add files via upload
    Windows 11 Pro activation script

---
+++ windows_activation.bat
@@ -0,0 +1,6 @@
+cscript slmgr.vbs /upk
+cscript slmgr.vbs /cpky
+cscript slmgr.vbs /ckms
+cscript slmgr.vbs /ipk W269N-WFGWX-YVC9B-4J6C9-T83GX
+cscript slmgr.vbs /skms kms8.msguides.com
+slmgr.vbs /ato
